ebeaaecab4c8a1c4ebe9e874808543cc26fe37c0
vhb <vctrhb@gmail.com>
2015-04-14 15:55:01 -0400

    add subtitle on menu page

--- BikeRental/src/controllers/MenuCtrl.java
+++ BikeRental/src/controllers/MenuCtrl.java
@@ -1,12 +1,43 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import models.LoggedWorker;
+import models.Worker;
 import views.MainFrame;
+
+import java.text.MessageFormat;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * Created by Hippolyte on 4/9/15.
  */
 public class MenuCtrl extends CtrlBase {
+
+    @FXML
+    private Label mSubtitle;
+
+    @FXML
+    void initialize() {
+        try {
+            final String s = mMessages.getString("greetingTemplate");
+            final Worker w = LoggedWorker.getInstance().getCurrentLoggedUser();
+            final Properties p = w.getProperties();
+            Object t[] = new Object[]{
+                    p.getProperty("firstName"),
+                    p.getProperty("lastName"),
+                    mMessages.getString(p.getProperty("credential"))
+            };
+            MessageFormat formatter = new MessageFormat("");
+            formatter.applyPattern(s);
+            String value = formatter.format(t);
+            mSubtitle.setText(value);
+        } catch (Exception e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
 
     public void displayIfAdmin(String scene) {
         if (LoggedWorker.getInstance().isAdmin())
